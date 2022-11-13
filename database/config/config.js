8aeaf9b2d08d705106c8c5c2fd8865661369eb14
Luis Ángel Núñez <njluisangel@gmail.com>
2022-11-13 16:19:22 -0600

    fix env variable names, installed bcryptjs and multer

--- database/config/config.js
+++ database/config/config.js
@@ -15,10 +15,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_DEV_USERNAME || "root",
-    "password": process.env.DB_DEV_PASSWORD || null,
-    "database": process.env.DB_DEV_DATABASE || "database_production",
-    "host": process.env.DB_DEV_HOST || "127.0.0.1",
+    "username": process.env.DB_PROD_USERNAME || "root",
+    "password": process.env.DB_PROD_PASSWORD || null,
+    "database": process.env.DB_PROD_DATABASE || "database_production",
+    "host": process.env.DB_PROD_HOST || "127.0.0.1",
     "dialect": "mysql"
   }
 }
